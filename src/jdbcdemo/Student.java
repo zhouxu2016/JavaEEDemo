ef7bbf459f2fa0a5ad91300320414ec3047ea067
zhouxu2016 <441689584@qq.com>
2018-01-24 16:24:51 +0800

    changyuan第一次提交代码(changyuan)

--- src/jdbcdemo/Student.java
+++ src/jdbcdemo/Student.java
@@ -18,7 +18,7 @@ public class Student {
     private Date intime;
 
     public Student() {
-        System.out.println("bbb");
+        System.out.println("ccc");
     }
 
     public Student(String sno, String sname, String gender, String professional, String classis, String grade, int age, Date intime) {
