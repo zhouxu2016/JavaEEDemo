d56dac796f25b3e80ed066638cbc1a6532443fee
zhouxu2016 <441689584@qq.com>
2018-01-24 16:20:53 +0800

    zhouxu第二次提交代码(zhouxu)

--- src/jdbcdemo/Student.java
+++ src/jdbcdemo/Student.java
@@ -18,6 +18,7 @@ public class Student {
     private Date intime;
 
     public Student() {
+        System.out.println("aaa");
     }
 
     public Student(String sno, String sname, String gender, String professional, String classis, String grade, int age, Date intime) {
