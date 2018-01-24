c4e495b1895b5f23557494459b3fb276a4f8cc6f
zhouxu2016 <441689584@qq.com>
2018-01-24 16:27:17 +0800

    changyuan第二次提交代码(changyuan)

--- src/jdbcdemo/Student.java
+++ src/jdbcdemo/Student.java
@@ -18,7 +18,7 @@ public class Student {
     private Date intime;
 
     public Student() {
-        System.out.println("qqq");
+        System.out.println("lll");
     }
 
     public Student(String sno, String sname, String gender, String professional, String classis, String grade, int age, Date intime) {
